15b5b945ea51885c51c18cd6d138436657672693
Mauricio-Millan <114685502+Mauricio-Millan@users.noreply.github.com>
2022-11-14 09:50:53 -0500

    Subida Inicial

---
+++ Fmpah/src/main/java/pe/com/fmpah/FmpahApplication.java
@@ -0,0 +1,13 @@
+package pe.com.fmpah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmpahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmpahApplication.class, args);
+	}
+
+}
---
+++ Fmpah/src/test/java/pe/com/fmpah/FmpahApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.fmpah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmpahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
